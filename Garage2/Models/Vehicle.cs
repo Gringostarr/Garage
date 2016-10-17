a9b1f0c4d9f6cbd417b14abd59e2337f8909ab81
Gragru <micke@primat.se>
2016-10-17 16:30:53 +0200

    Inget

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -13,25 +13,8 @@ namespace Garage2.Models
         public string Color { get; set; }
         [Display(Name = "Wheels")]
         public int NumberOfWheels { get; set; }
-        public int Wingspan { get; set; }
-        [Display(Name = "No. of propellers")]
-        public int NumberOfPropellers { get; set; }
-        [Display(Name = "Muffler")]
-        public bool HasMuffler { get; set; }
-        [Display(Name = "cm³")]
-        public int CubicCentimeter { get; set; }
-        [Display(Name = "hp")]
-        public int HorsePower { get; set; }
-        [Display(Name = "Cabriolet")]
-        public bool IsCabriolet { get; set; }
-        [Display(Name = "Capacity")]
-        public int PassengerCapacity { get; set; }
-        public int BusNumber { get; set; }
-        public int MaxKnotSpeed { get; set; }
-        public bool isSailingboat { get; set; }
         public VehicleType VehicleType { get; set; }
         public DateTime Checkin { get; set; }
-        public DateTime Checkout { get; set; }
 
     }
     public enum VehicleType
