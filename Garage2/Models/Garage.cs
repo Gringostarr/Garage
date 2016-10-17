877013dce5f257696cd28e29644bd984ed3f4dcd
Gragru <micke@primat.se>
2016-10-17 12:01:36 +0200

    Models created

---
+++ Garage2/Models/Garage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    // The Vehicles is stored here
+    public class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        private T[] vehicle;
+        public int Capacity { get; set; }
+        public int Count { get; private set; }
+        public int Id { get; set; }
+        //public string Name { get; set; }
+        //public int Placing { get; set; }
+
+        public Garage(int capacity)
+        {
+            Capacity = capacity;
+            vehicle = new T[capacity];
+        }
+
+        public bool AddVehicle(T item)
+        {
+            if (Count >= Capacity)
+            {
+                return false;
+            }
+            vehicle[Count] = item;
+            Count++;
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return vehicle[i];
+            }
+        }
+
+    }
+}
---
+++ Garage2/Models/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Garage2.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string Regnr { get; set; }
+        public string Color { get; set; }
+        public int NumberOfWheels { get; set; }
+        public int Wingspan { get; set; }
+        public int NumberOfPropellers { get; set; }
+        public bool HasMuffler { get; set; }
+        public int CubicCentimeter { get; set; }
+        public int HorsePower { get; set; }
+        public bool IsCabriolet { get; set; }
+        public int PassengerCapacity { get; set; }
+        public int BusNumber { get; set; }
+        public int MaxKnotSpeed { get; set; }
+        public bool isSailingboat { get; set; }
+        public VehicleType VehicleType { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+    }
+    public enum VehicleType
+    {
+        Car,
+        Airplane,
+        Boat,
+        Bus,
+        Motorcycle
+    }
+
+
+
+}
